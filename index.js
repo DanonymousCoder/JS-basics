fa0291c06b3085ddf55a71c0988f3aaceffebfe4
Dmysterius <128502434+DmysteriusCoder@users.noreply.github.com>
2024-09-05 18:26:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -171,3 +171,21 @@ function testElseIf(val) {
     return "Middle"
   }
 }  
+
+// First task - write a chained if/els statement
+
+function taskOne(val) {
+    if (val < 5) {
+        return "Tiny";
+    } else if (val < 10) {
+        return "Small";
+     }   else if (val < 15) {
+        return "Medium";
+     } else if (val < 20) {
+        return "Large";
+     } else {
+        return "Huge";
+     }
+} 
+
+console.log(taskOne(7));
